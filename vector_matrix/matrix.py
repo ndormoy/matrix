d0c99a8e508564805d9ed5e0a63aec03c9758bcd
Noe Dormoy <noedormoy@macbook-pro-de-noe.home>
2023-09-25 18:45:40 +0200

    ex10 doc

--- vector_matrix/matrix.py
+++ vector_matrix/matrix.py
@@ -92,6 +92,20 @@ class Matrix:
         - Leading Entries are 1: Each leading entry must be equal to 1, which means that if a row contains a leading entry, it should be divided by that leading entry to make it 1.
         - Leading Entry Column Zeros: All entries below and above a leading entry must be zeros.
         - Leading Entry Rows Sorted: Rows with all zeros should be placed at the bottom, and the rows with leading entries should be ordered so that the leading entry in each row is to the right of the leading entry in the row just above it.
+        
+        1) num_rows, num_cols = len(self.data), len(self.data[0]): This line calculates the number of rows and columns in the matrix self. It uses the len function to determine the length of self.data, which represents the rows, and the length of self.data[0], which represents the number of columns.
+        2) row = 0: Initialize a variable row to keep track of the current row during the row echelon form transformation. It starts at the first row (index 0).
+        3) The method enters a loop that iterates over each column (col) from left to right.
+        a. if row >= num_rows:: This condition checks whether we have processed all rows. If row is greater than or equal to the total number of rows (num_rows), it means we have completed the transformation for all rows, so the loop is exited using break.
+        b. pivot_row = row: Initialize pivot_row to the current row row.
+        c. while pivot_row < num_rows and self.data[pivot_row][col] == 0:: This loop searches for the first non-zero element in the current column (col). It keeps incrementing pivot_row until it finds a non-zero element or until it reaches the end of the matrix (num_rows). If it finds a non-zero element, the loop exits.
+        d. if pivot_row == num_rows:: If the loop in step c didn't find a non-zero element in the current column, this condition checks if we've reached the end of the matrix (num_rows) without finding a non-zero element. If so, it means the column is all zeros, and we move to the next column by using continue, skipping the remaining code for the current column.
+        e. self.swap_rows(pivot_row, row): If we found a non-zero element, we swap the rows to make this non-zero element the leading 1 (pivot element). This helps in creating the row echelon form. The swap_rows method is assumed to correctly swap the rows.
+        f. pivot_element = self.data[row][col]: The pivot_element variable is assigned the value of the leading 1 (pivot element) in the current row (row) and current column (col).
+        g. self.data[row] = [round(x / pivot_element, decimal_places) for x in self.data[row]]: We divide the entire row (self.data[row]) by the pivot_element to make it equal to 1. The round function is used to round each element in the row to the specified number of decimal places (decimal_places).
+        h. Next, we eliminate other rows by subtracting multiples of the current row from them. This ensures that the leading 1 in the current column is the only non-zero element in that column.
+        i. The loop variable row is incremented to move to the next row.
+        4) After processing all columns and rows, the method returns self, which now contains the matrix in row echelon form.
     """
     
     def swap_rows(self, row1, row2):
